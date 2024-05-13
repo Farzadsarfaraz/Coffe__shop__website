b39d23984a2f461511a1eb6159cfe68829c0fd23
Farzad_Sarfaraz <Farzadsarfarazwvo@gmail.com>
2024-05-13 13:53:00 +0200

    Navbar for mobile was actived.

--- new.js
+++ new.js
@@ -1,9 +1,15 @@
 const Hamburger = document.getElementById("Hamburger");
-const ulnav = document.querySelector(".ulnav");
+const ulnav = document.querySelector(".hidden");
+const closeNav = document.getElementById("closenav");
 
 
 Hamburger.addEventListener('click', myfunction);
 
 function myfunction(){
-    ulnav.classList.toggle(".hidden");
+    ulnav.classList.toggle("hidden");
+}
+closeNav.addEventListener('click', Myfunction);
+
+function Myfunction(){
+    ulnav.classList.toggle("hidden");
 }
